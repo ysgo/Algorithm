9708c27c2d9f177c26bcb03e58ebce00a52469f0
ysgo <wjion8428@gmail.com>
2019-08-20 19:32:11 +0900

    unsolved

---
+++ beatcoding/src/hard/recursive/Problem252.java
@@ -0,0 +1,13 @@
+package hard.recursive;
+
+import java.util.Scanner;
+
+public class Problem252 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		
+		
+		stdIn.close();
+	}
+}
---
+++ beatcoding/src/hard/recursive/Problem262.java
@@ -0,0 +1,13 @@
+package hard.recursive;
+
+import java.util.Scanner;
+
+public class Problem262 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		
+		
+		stdIn.close();
+	}
+}
