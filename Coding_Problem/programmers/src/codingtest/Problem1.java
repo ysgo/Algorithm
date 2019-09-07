04e9214277bc3410cdd97fb51e259d956252f109
ysgo <wjion8428@gmail.com>
2019-09-08 01:13:26 +0900

    solved

--- Coding_Problem/programmers/src/codingtest/Problem1.java
+++ Coding_Problem/programmers/src/codingtest/Problem1.java
@@ -1,96 +1,84 @@
 package codingtest;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Problem1 {
 	public static void main(String[] args) {
 		SolutionProblem1 su = new SolutionProblem1();
-		String s = "aabbaccc"; // 7
+//		String s = "aabbaccc"; // 7
 //		String s = "ababcdcdababcdcd";	// 9
 //		String s = "abcabcdede";	// 8
-//		String s = "abcabcabcabcdededededede";	// 14
+		String s = "abcabcabcabcdededededede";	// 14
 //		String s = "xababcdcdababcdcd";	// 17
 		System.out.println("answer : " + su.solution(s));
 	}
 }
-
+class ChangeString {
+	int cnt;
+	String str;
+	ChangeString(String str) {
+		this.cnt = 1;
+		this.str = str;
+	}
+}
 class SolutionProblem1 {
 	static int answer;
-	static HashMap<String, Integer> map;
-	static StringBuffer bf;
-
+	static List<ChangeString> list;
 	public int solution(String s) {
-		int size = 0;
 		answer = s.length();
-		map = new HashMap<>();
-		recur(s, 2, size);
+		recur(s, 1);
 		return answer;
 	}
-	static void recur(String s, int n, int size) {
-		bf = new StringBuffer();
+	static void recur(String s, int n) {
+		if(n >= 2 && !s.substring(n).contains(s.substring(0, n))) return;
+		list = new ArrayList<>();
 		int cur = 0; int prev = 0;
 		String curStr = "";
-		String preStr = "";
+//		String preStr = "";
 		for(int i=0; i<s.length(); i++) {
 			cur = i*n;
-			curStr = s.substring(cur, cur+n);
-			preStr = s.substring(prev, prev+n);
-			System.out.println("i : " + i + ", cur : " + cur + ", (cur+n) : " + (cur+n));
-			if(s.substring(prev+1).contains(curStr)) {
-				
-			}
-			bf.append(curStr);
-			System.out.println(bf.length());
-			prev = cur;
-			if(bf.length() == s.length()) {
+			if(cur > s.length()|| cur+n > s.length()) {
+				if(!s.substring(cur).isEmpty()) {
+					list.add(new ChangeString(s.substring(cur)));
+				}
 				break;
 			}
-		}
-		System.out.println(bf.toString());
-	}
-	
-	static int getSize(int size) {
-		for(String data : map.keySet())
-			size += map.get(data);
-		return size;
-	}
-	
-	static void splitString(String s, int splitLength, int size) {
-		if(answer < size && splitLength > s.length()/2-1) {
-			return;
-		}
-		int cur = 0; int prev = 0;
-		String curStr = "";
-		String preStr = "";
-		for(int i=0; i<s.length(); i++) {
-			cur = i*splitLength;
-			curStr = s.substring(cur, cur+splitLength);
-			preStr = s.substring(prev, prev+splitLength);
-			System.out.println(i + " " + cur + " " + (cur+splitLength));
-			if(i != 0) {
-				if(s.substring(cur).length() >= splitLength) {
-					if(curStr.equals(preStr)) {
-						map.put(curStr, map.get(curStr)+1);
-					} else {
-						if(map.containsKey(curStr)) {
-							map.put(curStr, map.get(curStr));
-						} else 
-							map.put(curStr, 1);
-					}
+//			System.out.println("prev : " + prev + ", cur : " + cur + ", (cur+n) : " + (cur+n));
+			curStr = s.substring(cur, cur+n);
+//			preStr = s.substring(prev, prev+n);
+//			System.out.println("i : " + i + ", cur : " + cur + ", (cur+n) : " + (cur+n));
+			if(!list.isEmpty()) {
+//				System.out.println("prev str : " + list.get(prev).str);
+				if(list.get(prev).str.equals(curStr)) {
+					list.get(prev).cnt++;
 				} else {
-					map.put(s.substring(cur), 1);
+					list.add(new ChangeString(curStr));
+					prev++;
 				}
+//				System.out.println(list.get(prev).cnt + " " + list.get(prev).str);
 			} else {
-				map.put(curStr, 1);
+				list.add(new ChangeString(curStr));
 			}
-			System.out.println(map);
-			prev = cur;
 		}
-		size = getSize(size);
-		if(answer > size) {
-			answer = size;
+//		for(int i=0; i<list.size(); i++) {
+//			System.out.println(list.get(i).cnt + " " + list.get(i).str);
+//		}
+		length();
+		recur(s, n+1);
+	}
+	static void length() {
+		ChangeString tmp;
+		StringBuffer bf = new StringBuffer();
+		for(int i=0; i<list.size(); i++) {
+			tmp = list.get(i);
+			if(tmp.cnt == 1) {
+				bf.append(tmp.str);
+			} else {
+				bf.append(tmp.cnt + tmp.str);
+			}
 		}
-		map.clear();
-		splitString(s, splitLength+1, size);
+//		System.out.println(bf.toString());
+		answer = answer > bf.length()? bf.length() : answer;
 	}
 }
